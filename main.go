710f7c912eab65693824ba70628a627c653b2241
Delevin Rero <delevinrero@gmail.com>
2024-06-06 16:19:45 +0700

    По своим задачам написал функцию main и положил начало разделениям функций

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"fmt"
+)
+
+const (
+	lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+	uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digits           = "0123456789"
+	specialChars     = "!@#$%^&*()-_=+[]{}|;:,.<>?/~`"
+)
+
 func main() {
+	password, err := generatePassword(12)
+	if err != nil {
+		fmt.Println("Ошибка в создании пароля:", err)
+		return
+	}
+
+	fmt.Println("Сгенерированный пароль:", password)
+}
+
+func generatePassword(length int) (string, error) {
+	//код
+	return "", fmt.Errorf("Длина пароля должна быть больше чем 4 символа")
 }
